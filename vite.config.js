6462f09b4dd8a362d3cea1a603a88d31245cc4f3
kiranjeet28 <kiranjeetkour144@gmail.com>
2024-09-22 17:49:36 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://kiranjeet28.github.io/3D-cylinder-/",
+  base: "./",
 });
