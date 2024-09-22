f111a51d9f5d530f65810926e81ecf04e5f89c0e
kiranjeet28 <kiranjeetkour144@gmail.com>
2024-09-22 17:42:11 +0530

    add workflow

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Canvas } from '@react-three/fiber'
 import './App.css'
 import { OrbitControls } from '@react-three/drei'
 import { Scene } from './Scene.jsx'
-import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing'
+import { Bloom, EffectComposer} from '@react-three/postprocessing'
 function App() {
  
   return (
